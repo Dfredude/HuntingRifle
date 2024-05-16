0e8d2ea9bd3c3ae68eec7cde308984c4a8a5dcfb
Alfredo Dominguez <thecatura.ad@gmail.com>
2024-05-16 10:52:34 -0300

    Add: context method for checking if a job has been already applied to

--- src/main/java/org/example/JobsContext.java
+++ src/main/java/org/example/JobsContext.java
@@ -17,4 +17,25 @@ public class JobsContext {
             System.out.println(e.getMessage());
         }
     }
+
+    public boolean hasBeenAppliedTo(String jobID) {
+        String sqlSelectJob = "SELECT * FROM jobs WHERE ID = " + jobID;
+        String connectionUrl = "jdbc:mysql://localhost:3306/job_apply_bot?serverTimezone=UTC";
+
+        try (Connection conn = DriverManager.getConnection(connectionUrl, "root", "thecatura");
+             PreparedStatement ps = conn.prepareStatement(sqlSelectJob)
+        ) {
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                System.out.println("Job has been applied to: " + jobID);
+                return true;
+            } else {
+                System.out.println("Job has not been applied to: " + jobID);
+                return false;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
 }
