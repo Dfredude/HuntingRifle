be8fd79ea016b3c30b4117e512ed58ecbf9e3c69
Alfredo Dominguez <thecatura.ad@gmail.com>
2024-05-16 10:54:11 -0300

    Add: AI answers/set cookie/get unwanted companies

--- src/main/java/org/example/DataTeller.java
+++ src/main/java/org/example/DataTeller.java
@@ -8,8 +8,8 @@ import java.util.List;
 import java.util.Map;
 
 public class DataTeller {
-    YamlReader reader;
-    Object data;
+    private YamlReader reader;
+    private Object cachedData;
     private List<String> unwanted_title_keywords = null;
     private Map<String, String> questions = null;
     private String cookie = null;
@@ -17,9 +17,9 @@ public class DataTeller {
     public DataTeller(){
         try {
             reader = new YamlReader(new FileReader("src/main/resources/data.yml"));
-            data = reader.read();
+            cachedData = reader.read();
             unwanted_title_keywords = getUnwantedKeywords();
-            questions = getQuestions();
+            questions = getCachedQuestions();
             cookie = getCookie();
             credentials = getCredentials();
 
@@ -37,7 +37,7 @@ public class DataTeller {
             if (credentials != null) {
                 return credentials;
             }
-            Map map = (Map) data;
+            Map map = (Map) cachedData;
             credentials = (Map) map.get("credentials");
             return credentials;
         } catch (Exception e) {
@@ -49,7 +49,7 @@ public class DataTeller {
 
     public String getCookie() {
         try {
-            Map map = (Map) data;
+            Map map = (Map) cachedData;
             return (String) map.get("cookie");
         } catch (Exception e) {
             e.printStackTrace();
@@ -58,12 +58,24 @@ public class DataTeller {
         return cookie;
     }
 
-    public Map<String, String> getQuestions(){
+    public boolean setCookie(String cookie){
+        try {
+            Map map = (Map) cachedData;
+            map.put("cookie", cookie);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Could not set cookie");
+        }
+        return false;
+    }
+
+    public Map<String, String> getCachedQuestions(){
         try {
             if (questions != null) {
                 return questions;
             }
-            Map map = (Map) data;
+            Map map = (Map) cachedData;
             questions = (Map) map.get("questions");
             return questions;
         } catch (Exception e) {
@@ -75,14 +87,16 @@ public class DataTeller {
 
 
     public String getAnswer(String question){
-        try {
-            questions = getQuestions();
-            return questions.get(question);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Could not find answer to question:\n" + question);
+        questions = getCachedQuestions();
+        String answer = questions.get(question);
+        if (answer != null){
+            return answer;
         }
-        return null;
+        System.out.println("Could not find cached answer to question:\n" + question);
+        // Fetch answer from generative model
+        answer = CVAI.ask(question);
+        System.out.println("Fetched answer from generative model:\n" + answer);
+        return answer;
     }
 
     public List<String> getUnwantedKeywords(){
@@ -90,11 +104,22 @@ public class DataTeller {
             return unwanted_title_keywords;
         }
         try {
-            Map map = (Map) data;
+            Map map = (Map) cachedData;
             return (List<String>) map.get("unwanted_title_keywords");
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Could not find unwanted_title_keywords");
+        }
+        return null;
+    }
+
+    public List<String> getUnwantedCompanies(){
+        try {
+            Map map = (Map) cachedData;
+            return (List<String>) map.get("unwanted_companies");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Could not find unwanted_companies");
         }
         return null;
     }
